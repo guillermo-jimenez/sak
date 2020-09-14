0e920b2125425b53ba1dee6163ee6008b5eae78b
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-09-14 16:25:26 +0200

    Renaming for abstraction

--- utils/data/__ops.py
+++ utils/data/__ops.py
@@ -2,6 +2,7 @@ from typing import Any
 from typing import List
 from typing import Tuple
 from typing import Callable
+import numpy as np
 
 class Struct:
     def __init__(self, structure: dict = {}, **kwargs):
@@ -9,9 +10,9 @@ class Struct:
         for key, value in structure.items():
             setattr(self, key, value)
 
-def ball_scaling(self, segment: np.ndarray, radius: float = 1.0):
+def ball_scaling(self, X: np.ndarray, radius: float = 1.0):
     """Balls of radius != 1 not implemented yet"""
     if radius != 1.0:
         raise NotImplementedError("Balls of radius != 1 not implemented yet")
-    return segment/(np.max(segment)-np.min(segment)+self.eps)
+    return X/(np.max(X)-np.min(X)+np.finfo(X.dtype).eps)
 
