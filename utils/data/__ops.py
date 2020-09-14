c097932d1c2bd309999f434d64c8fe01d9ddd20e
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-09-14 18:34:36 +0200

    Added distance metric

--- utils/data/__ops.py
+++ utils/data/__ops.py
@@ -10,9 +10,9 @@ class Struct:
         for key, value in structure.items():
             setattr(self, key, value)
 
-def ball_scaling(self, X: np.ndarray, radius: float = 1.0):
+def ball_scaling(X: np.ndarray, metric: Callable = lambda x: np.max(x)-np.min(x), radius: float = 1.0):
     """Balls of radius != 1 not implemented yet"""
     if radius != 1.0:
         raise NotImplementedError("Balls of radius != 1 not implemented yet")
-    return X/(np.max(X)-np.min(X)+np.finfo(X.dtype).eps)
+    return X/(metric(X)+np.finfo(X.dtype).eps)
 
