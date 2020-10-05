114ce3c1ec85c905fb113c24cb82d8a0171711a7
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-10-05 23:15:36 +0200

    More flexible data loader and saver

--- utils/__ops.py
+++ utils/__ops.py
@@ -1,32 +1,40 @@
 from typing import Any, Tuple, List
+import pandas as pd
 import numpy as np
 import pickle
 import importlib
 
 # Data loader to un-clutter code    
-def load_data(file):
+def load_data(file, dtype=int):
     dic = dict()
     with open(file) as f:
         text = list(f)
     for line in text:
-        line = line.replace(' ','').replace('\n','').replace(',,','')
-        if line[-1] == ',': line = line[:-1]
-        head = line.split(',')[0]
-        tail = line.split(',')[1:]
-        if tail == ['']:
+        line = line.replace(" ","").replace("\n","").replace(",,","").replace("'","")
+        if line[-1] == ",": line = line[:-1]
+        head = line.split(",")[0]
+        tail = line.split(",")[1:]
+        if tail == [""]:
             tail = np.asarray([])
         else:
-            tail = np.asarray(tail).astype(int)
+            tail = np.asarray(tail)
+            if dtype is not None:
+                tail = tail.astype(dtype)
 
         dic[head] = tail
     return dic
 
 # Data loader to un-clutter code    
-def save_data(dic,file):
-    with open(file, 'w') as f:
+def save_data(filepath, dic):
+    with open(filepath, 'w') as f:
         for key in dic.keys():
             # f.write("%s,%s\n"%(key,dic[key].tolist()))
-            f.write("{},{}\n".format(key,str(dic[key].tolist()).replace(']','').replace('[','').replace(' ','')))
+            iterable = dic[key]
+            if isinstance(iterable,pd.DataFrame):
+                iterable = iterable.values
+            if isinstance(iterable,np.ndarray):
+                iterable = iterable.tolist()
+            f.write("{},{}\n".format(key,str(iterable).replace(']','').replace('[','').replace(' ','')))
 
 def map_upper(lst: list) -> list:
     return list(map(str.upper,lst))
@@ -45,36 +53,36 @@ def check_header(header):
     if np.any((header[:,None] == np.unique(header)).sum(0) != 1):
         raise ValueError("Header with repeated entries not allowed")
 
-def channel2index(channel: str, header=['I', 'II', 'III', 'AVR', 'AVL', 'AVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']) -> int:
+def channel2index(channel: str, header=["I", "II", "III", "AVR", "AVL", "AVF", "V1", "V2", "V3", "V4", "V5", "V6"]) -> int:
     if not isinstance(channel, str):
-        raise ValueError("Channel '{}' type must be string. Provided type: {}.".format(channel,type(channel)))
+        raise ValueError("Channel {} type must be string. Provided type: {}.".format(channel,type(channel)))
     check_header(header)
     header = list(map(str.upper,header))
     location = np.where(np.array(header) == channel.upper())[0]
     if len(location) == 0:
-        raise ValueError("Channel '{}' not found in header with channels {}".format(channel, header))
+        raise ValueError("Channel {} not found in header with channels {}".format(channel, header))
     return np.where(np.array(header) == channel.upper())[0][0]
     
-def get_tqdm(iterator: iter, type: str = 'tqdm', **kwargs) -> Any:
+def get_tqdm(iterator: iter, type: str = "tqdm", **kwargs) -> Any:
     try:
-        iterator = class_selector('tqdm', type)(iterator, **kwargs)
+        iterator = class_selector("tqdm", type)(iterator, **kwargs)
     except:
         iterator = iterator
 
     return iterator
 
-def pickledump(obj: Any, file: str, mode: str = 'wb'):
+def pickledump(obj: Any, file: str, mode: str = "wb"):
     with open(file, mode) as f:
         pickle.dump(obj, f)
 
-def pickleload(file: str, mode: str = 'rb') -> Any:
+def pickleload(file: str, mode: str = "rb") -> Any:
     with open(file, mode) as f:
         obj = pickle.load(f)
     return obj
 
 def class_selector(module_name: str, class_name: str):
     # load the module, will raise ImportError if module cannot be loaded
-    if class_name == 'None':
+    if class_name == "None":
         class_name = class_name.lower()
     m = importlib.import_module(module_name)
     # return the class, will raise AttributeError if class cannot be found
@@ -89,6 +97,6 @@ class _RequiredParameter(object):
 def check_required(obj, dic: dict):
     for (param, value) in dic.items():
         if value is required:
-            raise ValueError("Class {} instantiated without required parameter '{}'".format(obj.__class__, param))
+            raise ValueError("Class {} instantiated without required parameter {}".format(obj.__class__, param))
 
 required = _RequiredParameter()
