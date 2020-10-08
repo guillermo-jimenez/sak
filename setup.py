dd68b262585aa2d7f1a509dfb83dac49d39fbe09
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-10-08 14:35:40 +0200

    Updated setup.py's requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 
 setup(
     name="sak",
-    description="General utils",
+    description="General utilitiles",
     url="https://github.com/gjimenez/sak",
     author="Guillermo Jimenez-Perez",
     author_email=" guillermo.jim.per@gmail.com \n guillermo.jimenez@upf.edu",
     # Needed to actually package something
     packages=["utils"],
     # Needed for dependencies
-    install_requires=["numpy", "scipy", "matplotlib"],
+    install_requires=["numpy", "scipy", "pandas", "wfdb", "dill", 
+                      "torch", "torchvision", "tqdm", "scikit-image", 
+                      "networkx", "defusedxml"],
     # *strongly* suggested for sharing
     version="0.1",
     # The license can be anything you like
