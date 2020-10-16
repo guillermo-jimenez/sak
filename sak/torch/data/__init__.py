0792f75b3c7f8291db484ec0465e4c4861e97afd
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-10-16 12:26:54 +0200

    Deleted DatasetComposer (deprecated)

--- sak/torch/data/__init__.py
+++ sak/torch/data/__init__.py
@@ -1,6 +1,5 @@
 from .__ops import Dataset
 from .__ops import DatasetWFDB
-from .__ops import DatasetComposer
 from .__ops import StratifiedSampler
 from .__ops import StratifiedSamplerWFDB
 from .__ops import UniformMultiDataset
