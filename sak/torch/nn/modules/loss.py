fca2fbf9353be3aaa3d319eb43b988c160a831ee
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-11-13 15:58:53 +0100

    overwrote input and target when sigmoiding

--- sak/torch/nn/modules/loss.py
+++ sak/torch/nn/modules/loss.py
@@ -489,24 +489,24 @@ class InstanceLoss2d(torch.nn.Module):
         self.prewitty = self.prewitty.to(target.device)
 
         # Obtain sigmoid-ed input and target
-        input_sigmoid  = self.sigmoid((input-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
-        target_sigmoid = self.sigmoid((target-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
+        input  = self.sigmoid((input-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
+        target = self.sigmoid((target-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
         
         # Obtain number of structures of the target
-        target_bound_x    = self.prewittx(target_sigmoid).abs()
+        target_bound_x    = self.prewittx(target).abs()
         target_bound_x    = self.sigmoid((target_bound_x-0.5)*self.threshold)
         target_elements_x = self.prewitt(target_bound_x.sum(-2)).abs().sum(-1)/(4**2)
         
-        target_bound_y    = self.prewitty(target_sigmoid).abs()
+        target_bound_y    = self.prewitty(target).abs()
         target_bound_y    = self.sigmoid((target_bound_y-0.5)*self.threshold)
         target_elements_y = self.prewitt(target_bound_y.sum(-1)).abs().sum(-1)/(4**2)
         
         # Obtain number of structures of the input
-        input_bound_x     = self.prewittx(input_sigmoid).abs()
+        input_bound_x     = self.prewittx(input).abs()
         input_bound_x     = self.sigmoid((input_bound_x-0.5)*self.threshold)
         input_elements_x  = self.prewitt(input_bound_x.sum(-2)).abs().sum(-1)/(4**2)
         
-        input_bound_y     = self.prewitty(input_sigmoid).abs()
+        input_bound_y     = self.prewitty(input).abs()
         input_bound_y     = self.sigmoid((input_bound_y-0.5)*self.threshold)
         input_elements_y  = self.prewitt(input_bound_y.sum(-1)).abs().sum(-1)/(4**2)
 
@@ -589,24 +589,24 @@ class F1InstanceLoss2d(torch.nn.Module):
         self.prewitty = self.prewitty.to(target.device)
 
         # Obtain sigmoid-ed input and target
-        input_sigmoid  = self.sigmoid((input-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
-        target_sigmoid = self.sigmoid((target-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
+        input  = self.sigmoid((input-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
+        target = self.sigmoid((target-0.5)*self.threshold) # Rule of thumb for dividing the classes as much as possible
         
         # Obtain number of structures of the target
-        target_bound_x    = self.prewittx(target_sigmoid).abs()
+        target_bound_x    = self.prewittx(target).abs()
         target_bound_x    = self.sigmoid((target_bound_x-0.5)*self.threshold)
         target_elements_x = self.prewitt(target_bound_x.sum(-2)).abs().sum(-1)/(4**2)
         
-        target_bound_y    = self.prewitty(target_sigmoid).abs()
+        target_bound_y    = self.prewitty(target).abs()
         target_bound_y    = self.sigmoid((target_bound_y-0.5)*self.threshold)
         target_elements_y = self.prewitt(target_bound_y.sum(-1)).abs().sum(-1)/(4**2)
         
         # Obtain number of structures of the input
-        input_bound_x     = self.prewittx(input_sigmoid).abs()
+        input_bound_x     = self.prewittx(input).abs()
         input_bound_x     = self.sigmoid((input_bound_x-0.5)*self.threshold)
         input_elements_x  = self.prewitt(input_bound_x.sum(-2)).abs().sum(-1)/(4**2)
         
-        input_bound_y     = self.prewitty(input_sigmoid).abs()
+        input_bound_y     = self.prewitty(input).abs()
         input_bound_y     = self.sigmoid((input_bound_y-0.5)*self.threshold)
         input_elements_y  = self.prewitt(input_bound_y.sum(-1)).abs().sum(-1)/(4**2)
 
