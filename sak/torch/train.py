09f11ce0c7bfbafa30b9b6531d792129fcce4fee
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2021-01-25 13:04:07 +0100

    Changed execution to config

--- sak/torch/train.py
+++ sak/torch/train.py
@@ -14,7 +14,7 @@ import torch.nn
 import numpy as np
 import sak.torch.data
 
-def do_epoch(model: torch.nn.Module, state: dict, execution: dict, 
+def do_epoch(model: torch.nn.Module, state: dict, config: dict, 
              dataloader: torch.utils.data.DataLoader, 
              criterion: Callable) -> list:
     """
@@ -29,29 +29,29 @@ def do_epoch(model: torch.nn.Module, state: dict, execution: dict,
     batch_loss = np.zeros((len(dataloader),),dtype='float16')
 
     # Create transforms
-    if ('data_pre' in execution):
-        data_pre = sak.from_dict(execution["data_pre"])
-    if ('augmentation' in execution) and model.training:
-        augmentation = sak.from_dict(execution["augmentation"])
-    if ('data_post' in execution):
-        data_post = sak.from_dict(execution["data_post"])
+    if ('data_pre' in config):
+        data_pre = sak.from_dict(config["data_pre"])
+    if ('augmentation' in config) and model.training:
+        augmentation = sak.from_dict(config["augmentation"])
+    if ('data_post' in config):
+        data_post = sak.from_dict(config["data_post"])
 
     # Select iterator decorator
     train_type = 'Train' if model.training else 'Valid'
-    iterator = sak.get_tqdm(dataloader, execution['iterator'], 
+    iterator = sak.get_tqdm(dataloader, config['iterator'], 
                             desc="({}) Epoch {:>3d}/{:>3d}, Loss {:0.3f}".format(train_type, 
                                                                                  state['epoch']+1, 
-                                                                                 execution['epochs'], np.inf))
+                                                                                 config['epochs'], np.inf))
 
     # Iterate over all data in train/validation/test dataloader:
     print_loss = np.inf
     for i, inputs in enumerate(iterator):
         # Apply data transforms
-        if ('data_pre' in execution):
+        if ('data_pre' in config):
             data_pre(inputs=inputs)
-        if ('augmentation' in execution) and model.training:
+        if ('augmentation' in config) and model.training:
             augmentation(inputs=inputs)
-        if ('data_post' in execution):
+        if ('data_post' in config):
             data_post(inputs=inputs)
 
         # Map all inputs to device
@@ -86,48 +86,48 @@ def do_epoch(model: torch.nn.Module, state: dict, execution: dict,
         # Change iterator description
         if isinstance(iterator,tqdm.tqdm):
             if i == len(iterator)-1:
-                iterator.set_description("({}) Epoch {:>3d}/{:>3d}, Loss {:10.3f}".format(train_type, state['epoch']+1, execution['epochs'], np.mean(batch_loss)))
+                iterator.set_description("({}) Epoch {:>3d}/{:>3d}, Loss {:10.3f}".format(train_type, state['epoch']+1, config['epochs'], np.mean(batch_loss)))
             else:
-                iterator.set_description("({}) Epoch {:>3d}/{:>3d}, Loss {:10.3f}".format(train_type, state['epoch']+1, execution['epochs'], print_loss))
+                iterator.set_description("({}) Epoch {:>3d}/{:>3d}, Loss {:10.3f}".format(train_type, state['epoch']+1, config['epochs'], print_loss))
             iterator.refresh()
 
     return batch_loss
 
 
-def train_model(model, state: dict, execution: dict, loader: torch.utils.data.DataLoader):
+def train_model(model, state: dict, config: dict, loader: torch.utils.data.DataLoader):
     # Send model to device
     model = model.to(state['device'])
 
     # Instantiate criterion
-    criterion = sak.from_dict(execution['loss'])
+    criterion = sak.from_dict(config['loss'])
     
     # Initialize best loss for early stopping
     if 'best_loss' not in state:
         state['best_loss'] = np.inf
 
     # Get savedir string
-    if "savedir" in execution:          str_savedir = 'savedir'
-    elif "save_directory" in execution: str_savedir = 'save_directory'
+    if "savedir" in config:          str_savedir = 'savedir'
+    elif "save_directory" in config: str_savedir = 'save_directory'
     else: raise ValueError("Configuration file should include either the 'savedir' or 'save_directory' fields [case-sensitive]")
 
     # Iterate over epochs
-    for epoch in range(state['epoch'], execution['epochs']):
+    for epoch in range(state['epoch'], config['epochs']):
         try:
             # Store current epoch
             state['epoch'] = epoch
             
             # Train model
-            loss_train = do_epoch(model.train(), state, execution, loader, criterion)
+            loss_train = do_epoch(model.train(), state, config, loader, criterion)
             state['loss_train'] = np.mean(loss_train)
 
             # Save model/state info
-            torch.save(model, os.path.join(execution[str_savedir],'checkpoint.model'), pickle_module=dill)
-            sak.pickledump(state, os.path.join(execution[str_savedir],'checkpoint.state'), mode='wb')
+            torch.save(model, os.path.join(config[str_savedir],'checkpoint.model'), pickle_module=dill)
+            sak.pickledump(state, os.path.join(config[str_savedir],'checkpoint.state'), mode='wb')
             
             # Log train loss
-            with open(os.path.join(execution[str_savedir],'log.csv'),'a') as f:
+            with open(os.path.join(config[str_savedir],'log.csv'),'a') as f:
                 csvwriter = csv.writer(f)
-                csvwriter.writerow(["(Train) Epoch {:>3d}/{:>3d}, Loss {:10.3f}, Time {}".format(state['epoch']+1, execution['epochs'], state['loss_train'], time.ctime())])
+                csvwriter.writerow(["(Train) Epoch {:>3d}/{:>3d}, Loss {:10.3f}, Time {}".format(state['epoch']+1, config['epochs'], state['loss_train'], time.ctime())])
 
             # Check if loss is best loss
             if state['loss_train'] < state['best_loss']:
@@ -135,49 +135,49 @@ def train_model(model, state: dict, execution: dict, loader: torch.utils.data.Da
                 state['best_epoch'] = epoch
                 
                 # Copy checkpoint and mark as best
-                shutil.copyfile(os.path.join(execution[str_savedir],'checkpoint.model'), os.path.join(execution[str_savedir],'model_best.model'))
-                shutil.copyfile(os.path.join(execution[str_savedir],'checkpoint.state'), os.path.join(execution[str_savedir],'model_best.state'))
+                shutil.copyfile(os.path.join(config[str_savedir],'checkpoint.model'), os.path.join(config[str_savedir],'model_best.model'))
+                shutil.copyfile(os.path.join(config[str_savedir],'checkpoint.state'), os.path.join(config[str_savedir],'model_best.state'))
         except KeyboardInterrupt:
-            torch.save(model, os.path.join(execution[str_savedir],'keyboard_interrupt.model'), pickle_module=dill)
-            sak.pickledump(state, os.path.join(execution[str_savedir],'keyboard_interrupt.state'), mode='wb')
+            torch.save(model, os.path.join(config[str_savedir],'keyboard_interrupt.model'), pickle_module=dill)
+            sak.pickledump(state, os.path.join(config[str_savedir],'keyboard_interrupt.state'), mode='wb')
             raise
         except:
-            torch.save(model, os.path.join(execution[str_savedir],'error.model'), pickle_module=dill)
-            sak.pickledump(state, os.path.join(execution[str_savedir],'error.state'), mode='wb')
+            torch.save(model, os.path.join(config[str_savedir],'error.model'), pickle_module=dill)
+            sak.pickledump(state, os.path.join(config[str_savedir],'error.state'), mode='wb')
             raise
 
 
-def train_valid_model(model, state: dict, execution: dict, 
+def train_valid_model(model, state: dict, config: dict, 
                       loader_train: torch.utils.data.DataLoader, 
                       loader_valid: torch.utils.data.DataLoader):
     # Send model to device
     model = model.to(state['device'])
 
     # Instantiate criterion
-    criterion = sak.from_dict(execution['loss'])
+    criterion = sak.from_dict(config['loss'])
     
     # Initialize best loss for early stopping
     if 'best_loss' not in state:
         state['best_loss'] = np.inf
 
     # Get savedir string
-    if "savedir" in execution:          str_savedir = 'savedir'
-    elif "save_directory" in execution: str_savedir = 'save_directory'
+    if "savedir" in config:          str_savedir = 'savedir'
+    elif "save_directory" in config: str_savedir = 'save_directory'
     else: raise ValueError("Configuration file should include either the 'savedir' or 'save_directory' fields [case-sensitive]")
 
     # Iterate over epochs
-    for epoch in range(state['epoch'], execution['epochs']):
+    for epoch in range(state['epoch'], config['epochs']):
         try:
             # Store current epoch
             state['epoch'] = epoch
             
             # Training model
-            loss_train = do_epoch(model.train(), state, execution, loader_train, criterion)
+            loss_train = do_epoch(model.train(), state, config, loader_train, criterion)
             state['loss_train'] = np.mean(loss_train)
 
             # Validate results
             with torch.no_grad():
-                loss_valid = do_epoch(model.eval(), state, execution, loader_valid, criterion)
+                loss_valid = do_epoch(model.eval(), state, config, loader_valid, criterion)
             state['loss_validation'] = np.mean(loss_valid)
 
             # Update learning rate scheduler
@@ -185,14 +185,14 @@ def train_valid_model(model, state: dict, execution: dict,
                 state['scheduler'].step(state['loss_validation'])
 
             # Save model/state info
-            torch.save(model, os.path.join(execution[str_savedir],'checkpoint.model'), pickle_module=dill)
-            sak.pickledump(state, os.path.join(execution[str_savedir],'checkpoint.state'), mode='wb')
+            torch.save(model, os.path.join(config[str_savedir],'checkpoint.model'), pickle_module=dill)
+            sak.pickledump(state, os.path.join(config[str_savedir],'checkpoint.state'), mode='wb')
             
             # Log train/valid losses
-            with open(os.path.join(execution[str_savedir],'log.csv'),'a') as f:
+            with open(os.path.join(config[str_savedir],'log.csv'),'a') as f:
                 csvwriter = csv.writer(f)
-                csvwriter.writerow(["(Train) Epoch {:>3d}/{:>3d}, Loss {:10.3f}, Time {}".format(state['epoch']+1, execution['epochs'], state['loss_train'], time.ctime())])
-                csvwriter.writerow(["(Valid) Epoch {:>3d}/{:>3d}, Loss {:10.3f}, Time {}".format(state['epoch']+1, execution['epochs'], state['loss_validation'], time.ctime())])
+                csvwriter.writerow(["(Train) Epoch {:>3d}/{:>3d}, Loss {:10.3f}, Time {}".format(state['epoch']+1, config['epochs'], state['loss_train'], time.ctime())])
+                csvwriter.writerow(["(Valid) Epoch {:>3d}/{:>3d}, Loss {:10.3f}, Time {}".format(state['epoch']+1, config['epochs'], state['loss_validation'], time.ctime())])
 
             # Check if loss is best loss
             compound_loss = 2*state['loss_train']*state['loss_validation']/(state['loss_train']+state['loss_validation'])
@@ -201,14 +201,14 @@ def train_valid_model(model, state: dict, execution: dict,
                 state['best_epoch'] = epoch
                 
                 # Copy checkpoint and mark as best
-                shutil.copyfile(os.path.join(execution[str_savedir],'checkpoint.model'), os.path.join(execution[str_savedir],'model_best.model'))
-                shutil.copyfile(os.path.join(execution[str_savedir],'checkpoint.state'), os.path.join(execution[str_savedir],'model_best.state'))
+                shutil.copyfile(os.path.join(config[str_savedir],'checkpoint.model'), os.path.join(config[str_savedir],'model_best.model'))
+                shutil.copyfile(os.path.join(config[str_savedir],'checkpoint.state'), os.path.join(config[str_savedir],'model_best.state'))
             
         except KeyboardInterrupt:
-            torch.save(model, os.path.join(execution[str_savedir],'keyboard_interrupt.model'), pickle_module=dill)
-            sak.pickledump(state, os.path.join(execution[str_savedir],'keyboard_interrupt.state'), mode='wb')
+            torch.save(model, os.path.join(config[str_savedir],'keyboard_interrupt.model'), pickle_module=dill)
+            sak.pickledump(state, os.path.join(config[str_savedir],'keyboard_interrupt.state'), mode='wb')
             raise
         except:
-            torch.save(model, os.path.join(execution[str_savedir],'error.model'), pickle_module=dill)
-            sak.pickledump(state, os.path.join(execution[str_savedir],'error.state'), mode='wb')
+            torch.save(model, os.path.join(config[str_savedir],'error.model'), pickle_module=dill)
+            sak.pickledump(state, os.path.join(config[str_savedir],'error.state'), mode='wb')
             raise
