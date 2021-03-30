bbc8df7cc71cb79ef3944ce4a29f6ef9353a2af6
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2021-03-30 17:36:45 +0200

    Introduced new modules into SAK

--- sak/torch/nn/__init__.py
+++ sak/torch/nn/__init__.py
@@ -1,18 +1,28 @@
 from torch.nn import *
-from .modules.loss import CompoundLoss, XCorrLoss, ConstantLoss, PearsonCorrelationLoss, KLD_MSE, KLD_BCE, KLDivergence, DiceLoss, BoundDiceLoss1d, F1InstanceLoss1d, BoundDiceLoss2d, F1InstanceLoss2d
-from .modules.models import CNN, DNN, DCC, Residual, SelfAttention
-from .modules.modules import ImagePooling1d,                  ImagePooling2d,                  ImagePooling3d, \
-                             PointwiseConv1d,                 PointwiseConv2d,                 PointwiseConv3d, \
-                             DepthwiseConv1d,                 DepthwiseConv2d,                 DepthwiseConv3d, \
-                             SeparableConv1d,                 SeparableConv2d,                 SeparableConv3d, \
-                             PointwiseConvTranspose1d,        PointwiseConvTranspose2d,        PointwiseConvTranspose3d, \
-                             DepthwiseConvTranspose1d,        DepthwiseConvTranspose2d,        DepthwiseConvTranspose3d, \
-                             SeparableConvTranspose1d,        SeparableConvTranspose2d,        SeparableConvTranspose3d, \
-                             SqueezeAndExcitation1d,          SqueezeAndExcitation2d,          SqueezeAndExcitation3d, \
-                             PointwiseSqueezeAndExcitation1d, PointwiseSqueezeAndExcitation2d, PointwiseSqueezeAndExcitation3d, \
-                             EfficientChannelAttention1d,     EfficientChannelAttention2d,     EfficientChannelAttention3d, \
-                             AdaptiveAvgPoolAttention1d,      AdaptiveAvgPoolAttention2d,      AdaptiveAvgPoolAttention3d, \
-                             PointwiseAttention1d,            PointwiseAttention2d,            PointwiseAttention3d, \
-                             RBF,                             SVM
-from .modules.composers import ModelGraph, Sequential, Parallel
-from .modules.utils import SoftArgmaxAlongAxis, Lambda, Concatenate, ViewAsWindows, ViewDimensionAsWindows, OperationIterator, Regularization, Reparameterize, none, Add, Multiply, Squeeze, Unsqueeze, View, UnFlatten, Dropout1d
+from .modules.loss import   CompoundLoss,                       XCorrLoss,                          ConstantLoss, \
+                            PearsonCorrelationLoss,             KLD_MSE,                            KLD_BCE, \
+                            KLDivergence,                       DiceLoss,                           BoundDiceLoss1d, \
+                            F1InstanceLoss1d,                   BoundDiceLoss2d,                    F1InstanceLoss2d
+from .modules.models import CNN,                                DNN,                                DCC, \
+                            Residual,                           SelfAttention
+from .modules.modules import ImagePooling1d,                    ImagePooling2d,                     ImagePooling3d, \
+                             PointwiseConv1d,                   PointwiseConv2d,                    PointwiseConv3d, \
+                             DepthwiseConv1d,                   DepthwiseConv2d,                    DepthwiseConv3d, \
+                             SeparableConv1d,                   SeparableConv2d,                    SeparableConv3d, \
+                             PointwiseConvTranspose1d,          PointwiseConvTranspose2d,           PointwiseConvTranspose3d, \
+                             DepthwiseConvTranspose1d,          DepthwiseConvTranspose2d,           DepthwiseConvTranspose3d, \
+                             SeparableConvTranspose1d,          SeparableConvTranspose2d,           SeparableConvTranspose3d, \
+                             SqueezeAndExcitation1d,            SqueezeAndExcitation2d,             SqueezeAndExcitation3d, \
+                             PointwiseSqueezeAndExcitation1d,   PointwiseSqueezeAndExcitation2d,    PointwiseSqueezeAndExcitation3d, \
+                             EfficientChannelAttention1d,       EfficientChannelAttention2d,        EfficientChannelAttention3d, \
+                             AdaptiveAvgPoolAttention1d,        AdaptiveAvgPoolAttention2d,         AdaptiveAvgPoolAttention3d, \
+                             PointwiseAttention1d,              PointwiseAttention2d,               PointwiseAttention3d, \
+                             RBF,                               SVM,                                CrossResidualFusionBlock
+from .modules.composers import ModelGraph,                      Sequential,                         Parallel
+from .modules.utils import  SoftArgmaxAlongAxis,                Lambda,                             Concatenate, \
+                            ViewAsWindows,                      ViewDimensionAsWindows,             OperationIterator, \
+                            Regularization,                     Reparameterize,                     none, \
+                            Add,                                Multiply,                           Squeeze, \
+                            Unsqueeze,                          View,                               UnFlatten, \
+                            Dropout1d,                          PrintShapes,                        Interpolate
+                             
