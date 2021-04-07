27904b8cadf3b54d8beb4a244a958beb00a0ac35
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2021-04-07 13:37:15 +0200

    Added Splitter and Pipeline executor

--- sak/data/__init__.py
+++ sak/data/__init__.py
@@ -2,3 +2,5 @@ import sak.data.augmentation
 from .__ops import Struct
 from .__ops import ball_scaling
 from .__ops import split_train_valid_test
+from .__ops import SplitterTrainValidTest
+from .__ops import Pipeline
