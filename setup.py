75debc0ac5073979cb4b01c49eaa6c51e5a33a15
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2021-05-03 09:29:44 +0200

    .

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="sak",
-    description="General utilitiles",
+    description="Swiss Army Knife - General utilitiles for signal and image processing",
     url="https://github.com/gjimenez/sak",
     author="Guillermo Jimenez-Perez",
-    author_email=" guillermo.jim.per@gmail.com \n guillermo.jimenez@upf.edu",
+    author_email="<guillermo@jimenezperez.com>",
     # Needed to actually package something
-    packages=["sak"],
+    packages=find_packages(),
     # Needed for dependencies
     install_requires=["numpy", "scipy", "pandas", "wfdb", "dill", 
                       "torch", "torchvision", "tqdm", "scikit-image", 
                       "networkx", "defusedxml"],
     # *strongly* suggested for sharing
-    version="0.1",
+    version="0.0.1",
     # The license can be anything you like
     classifiers=[
         "Programming Language :: Python :: 3",
